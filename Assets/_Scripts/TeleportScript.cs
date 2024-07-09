48fb96cf6eb6678245c692607304cc355fce0328
qunngh <hoquang1031@gmail.com>
2024-07-09 09:23:55 +0700

    expand map

---
+++ Assets/_Scripts/TeleportScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform destination;
+    GameObject player;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Vector2.Distance(player.transform.position, transform.position) > 0.3f)
+            {
+                player.transform.position = destination.transform.position;
+            }
+        }
+    }
+}
